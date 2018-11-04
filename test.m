47823e1bda9811ae00223324f9073a39a721301a
Alex B <alex-badea@github.com>
2018-11-04 21:06:20 +0200

    -

--- test.m
+++ test.m
@@ -1,10 +1,2 @@
-tic
-X12DN = FilterBackground(X12);
-toc
-figure,pcshow(X12DN','MarkerSize',120)
-Z = X12DN(3,:);
-figure('pos', [10 100 1350 300]),histogram(Z,2000)
-
-figure,pcshow(X12','MarkerSize',120)
-Z = X12(3,:);
-figure('pos', [10 100 1350 300]),histogram(Z,2000)
+im1 = imread('cks01.jpg');
+im2 = imread('csk02.jpg');
