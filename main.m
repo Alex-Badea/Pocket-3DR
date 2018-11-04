206145a1caeba277ec32123e53b9f9e287f020da
Alex B <alex-badea@github.com>
2018-11-04 21:00:19 +0200

    Full pipeline defective

--- main.m
+++ main.m
@@ -1,38 +1,121 @@
-addpath(genpath(fileparts(which(mfilename))))
+init
+dataset = 'csk';
 
-ds = 'fnt';
-im1 = imread([ds '01.jpg']);
-im2 = imread([ds '02.jpg']);
-im3 = imread([ds '03.jpg']);
-fp1 = EstimateFeaturePoints(im1);
-fp2 = EstimateFeaturePoints(im2);
-fp3 = EstimateFeaturePoints(im3);
-mc12 = MatchFeaturePoints(fp1,fp2);
+%% Reading image dataset
+imsDir = dir(['ims/' dataset '*.jpg']);
+imsNames = {imsDir.name};
+imsNo = length(imsNames);
+Cim = cell(1,imsNo);
+for i = 1:length(imsNames)
+    Cim{i} = imread(imsNames{i});
+end
 
-mc12n = [Dehomogenize(K\Homogenize(mc12(1:2,:))); 
-    Dehomogenize(K\Homogenize(mc12(3:4,:)))];
-mc23 = MatchFeaturePoints(fp2,fp3);
-mc23n = [Dehomogenize(K\Homogenize(mc23(1:2,:))); 
-    Dehomogenize(K\Homogenize(mc23(3:4,:)))]; 
-%%
-[E1,Cmc12nin] = RANSAC(num2cell(mc12n,1), @EstimateEssentialMatrix, ...
-    5, @SampsonDistance, 1e-6);
-mc12nin = cell2mat(Cmc12nin);
-[E2,Cmc23nin] = RANSAC(num2cell(mc23n,1), @EstimateEssentialMatrix, ...
-    5, @SampsonDistance, 1e-6);
-mc23nin = cell2mat(Cmc23nin);
-%% MINIM. EPIP. ERR.
-E1 = OptimizeEssentialMatrix(E1, mc12nin(1:2,:), mc12nin(3:4,:));
-E2 = OptimizeEssentialMatrix(E2, mc23nin(1:2,:), mc23nin(3:4,:));
-%%
-P1 = CANONICAL_POSE;
-[P2,X12] = EstimateRealPoseAndTriangulate(E1,mc12nin(1:2,:),mc12nin(3:4,:),P1);
-[P3,X23] = EstimateRealPoseAndTriangulate(E2,mc23nin(1:2,:),mc23nin(3:4,:),P2);
+%% Computing correspondences
+CfeatPts = cell(1,imsNo);
+for i = 1:imsNo
+    disp(['Feature points estimation: image ' num2str(i) ' of ' num2str(imsNo)])
+    CfeatPts{i} = EstimateFeaturePoints(Cim{i});
+end
+CcorrsNorm = cell(1,imsNo-1);
+for i = 1:imsNo-1
+    disp(['Feature matching: pair ' num2str(i) ' of ' num2str(imsNo-1)])
+    corrs = MatchFeaturePoints(CfeatPts{i}, CfeatPts{i+1});
+    CcorrsNorm{i} = [Dehomogenize(K\Homogenize(corrs(1:2,:)));
+        Dehomogenize(K\Homogenize(corrs(3:4,:)))];
+end
 
-PlotCorrespondences(im1,im2,mc12(1:2,:),mc12(3:4,:),...
-    Dehomogenize(K*Homogenize(mc12nin(1:2,:))),...
-    Dehomogenize(K*Homogenize(mc12nin(3:4,:))))
+%% Essential Matrix estimation
+CcorrsNormIn = cell(1,imsNo-1);
+CE = cell(1,imsNo-1);
+for i = 1:imsNo-1
+    disp(['Essential Matrix estimation: pair ' num2str(i) ' of ' num2str(imsNo-1)])
+    [NewE,Cinliers] = RANSAC(num2cell(CcorrsNorm{i},1), ...
+        @EstimateEssentialMatrix, 5, @SampsonDistance, 1e-7);
+    CcorrsNormIn{i} = cell2mat(Cinliers);
+    CE{i} = OptimizeEssentialMatrix(NewE, ...
+        CcorrsNormIn{i}(1:2,:), CcorrsNormIn{i}(3:4,:));
+end
 
-PlotCorrespondences(im2,im3,mc23(1:2,:),mc23(3:4,:),...
-    Dehomogenize(K*Homogenize(mc23nin(1:2,:))),...
-    Dehomogenize(K*Homogenize(mc23nin(3:4,:))))
+%% Structure from Motion
+disp('Pose estimation: default first pair')
+CP = cell(1,imsNo);
+CP{1} = CANONICAL_POSE;
+CP{2} = EstimateRealPoseAndTriangulate(CE{1}, ...
+    CcorrsNormIn{1}(1:2,:), CcorrsNormIn{1}(3:4,:));
+for i = 2:imsNo-1
+    disp(['Pose estimation: ' num2str(i+1) ' of ' num2str(imsNo)])
+    CP{i+1} = EstimateRealPoseAndTriangulate(CE{i}, ...
+        CcorrsNormIn{i}(1:2,:), CcorrsNormIn{i}(3:4,:), CP{i});
+    PrimeSubjectCorrs1 = FilterBackgroundFromCorrs(CP{i-1}, CP{i}, ...
+        CcorrsNormIn{i-1});
+    PrimeSubjectCorrs2 = FilterBackgroundFromCorrs(CP{i}, CP{i+1}, ...
+        CcorrsNormIn{i});
+    TrackedCorrs = CascadeTrack({PrimeSubjectCorrs1, PrimeSubjectCorrs2});
+    TrackedCorrsIso = IsolateTransitiveCorrs(TrackedCorrs);
+    %%%%%%%%%%%%%%%%%
+    disp(['trans ' num2str(size(TrackedCorrsIso,2))])
+    %%%%%%%%%%%%%%%%%%
+    %[CP{i+1},error] = OptimizeTranslationVector(CP{i-1}, CP{i}, CP{i+1}, ...
+    %    TrackedCorrsIso(1:2,:), TrackedCorrsIso(3:4,:), TrackedCorrsIso(5:6,:));
+    %[CP{i+1},error] = OptimizePose(CP{i-1}, CP{i}, CP{i+1}, ...
+    %    TrackedCorrsIso(1:2,:), TrackedCorrsIso(3:4,:), TrackedCorrsIso(5:6,:));
+    %disp(['err ' num2str(error)])
+end
+
+
+
+
+X = [];
+for i = 1:imsNo-1
+    X = [X Triangulate(CP{i},CP{i+1},CcorrsNormIn{i}(1:2,:),CcorrsNormIn{i}(3:4,:))];
+end
+pcshow(X','MarkerSize',50);
+axis([-5 5 -5 5 0 20])
+return
+%% Bundle Adjustment
+
+[X, CP] = BundleAdjustment(M,K,X,CP);
+
+CPExt = [nan(3,4) CP];
+Guide = (1:size(M,1)/2)'.* ~isnan(M(1:2:end,:));
+
+XExt = repmat(X,1,size(M,1)/2);
+Guide = reshape(Guide',1,numel(Guide));
+MRpj = bsxfun(@(X, camInd) K*CPExt{camInd+1}*Homogenize(X), XExt, Guide);
+MRpj = Dehomogenize(MRpj);
+MRpj = BlockReshape(MRpj,size(M,1)/2);
+ERRS = M-MRpj;
+ERRS(:,sum(~isnan(ERRS))>4);
+ERRS(isnan(ERRS)) = 0;
+disp(['Eroare dup? BA: ' num2str(vecnorm(ERRS)*vecnorm(ERRS)'/size(ERRS,2))]);
+%PlotSparse(CP, X)
+
+%% Dense reconstruction
+CX = cell(1,imsNo-1);
+for i = 1:imsNo-1
+    disp(['Dense reconstruction ' num2str(i) '/' num2str(imsNo-1)])
+    F = K'\CE{i,i+1}/K;
+    [H1, H2, im1Rec, im2Rec] = rectify(F, Cim{i}, Cim{i+1});
+    im1Rec(im1Rec==0) = -1;
+    KP1Rec = H1*K*CP{i};
+    KP2Rec = H2*K*CP{i+1};
+    
+    pars = [];
+    pars.mu = -10.6;
+    pars.window = 4;
+    pars.zonegap = 10;
+    pars.pm_tau = 0.95;
+    D = gcs(im1Rec, im2Rec, [], pars);
+    mc12Rec = CorrsFromDisparity(D);
+    x1Rec = mc12Rec(1:2,:);
+    x2Rec = mc12Rec(3:4,:);
+    CX{i} = LinearTriangulation(KP1Rec, KP2Rec, x1Rec, x2Rec);
+end
+%{
+%% Denoising the triangulated points
+for i = 1:imsNo-1
+    disp(['Denoising ' num2str(i) '/' num2str(imsNo-1)])
+    CX{i} = pcdenoise(pointCloud(CX{i}'),'Threshold',0.001,'NumNeighbors',500);
+    CX{i} = CX{i}.Location';
+end
+%}
