7f2ef9842e0a1c66b12b54ede6af62d4c516db5c
Alex B <alex-badea@github.com>
2018-11-04 20:26:13 +0200

    Full pipeline almost implemented

--- EstimateFeaturePoints.m
+++ EstimateFeaturePoints.m
@@ -1,7 +1,7 @@
 function [fPts] = EstimateFeaturePoints(im)
 %ESTIMATEFEATUREPOINTS Summary of this function goes here
 %   Detailed explanation goes here
-kPts = detectSURFFeatures(rgb2gray(im), 'MetricThreshold', 20);
+kPts = detectSURFFeatures(rgb2gray(im), 'MetricThreshold', 10);
 [feats,kPts] = extractFeatures(rgb2gray(im), kPts);
 fPts = {feats,kPts};
 end
